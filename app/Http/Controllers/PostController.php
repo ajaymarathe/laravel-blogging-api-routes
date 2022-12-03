54d57f29223d04b551e89cf84bdfcdc7965a0412
Ajay Marathe <ma143ajay@gmail.com>
2022-12-03 12:45:19 +0530

    return only 5 post for now.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $post = Post::latest()->get();
+        $post = Post::latest()->take(5)->get();;
         return PostResource::collection($post);
     }
 
